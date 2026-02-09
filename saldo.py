d5ed517617e949efd001c6b23af0d8432e779079
jelu000 <jelu000@gmail.com>
2026-02-09 15:57:05 +0100

    första commit

---
+++ saldo.py
@@ -0,0 +1,20 @@
+#Här börjar min kod, en läsare av
+import pandas as pd
+
+
+def main():
+
+    df = pd.read_csv("Transaktioner_jan2026.csv", encoding="latin-1", skiprows=1, sep=",")
+    swis_inb = df[df["Text"].str.startswith("Swish", na=False)]
+    
+    print(f'Inkomster:')
+    print(swis_inb)
+    print("\n Här kommer utgifter: \n")
+    # HÄMTAR ALLT UTOM SWISH:
+    icke_swish = df[~df["Text"].str.startswith("Swish", na=False)]
+
+    print(icke_swish)
+
+
+
+main()
