f7d5b27d0208ea789cfa17c748ed6a9e08fff3bd
jelu000 <jelu000@gmail.com>
2026-02-10 11:53:52 +0100

    fixade columner

--- saldo.py
+++ saldo.py
@@ -4,17 +4,22 @@ import pandas as pd
 
 def main():
 
+# gemini : Istället för att bara titta på texten "Swish", kan du använda df['Belopp'] < 0 f
+
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 1000)
+    
     df = pd.read_csv("../Transaktioner_2026-02-09_16-25-43.csv", encoding="latin-1", skiprows=1, sep=",")
     swis_inb = df[df["Text"].str.startswith("Swish", na=False)]
     
     print(f'Inkomster:')
+    #gemeny- print(swish_in['Belopp'].sum())
     print(swis_inb)
+    
     print("\n Här kommer utgifter: \n")
     # HÄMTAR ALLT UTOM SWISH:
     icke_swish = df[~df["Text"].str.startswith("Swish", na=False)]
 
     print(icke_swish)
-
-
 
 main()
